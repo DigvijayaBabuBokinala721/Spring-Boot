7cd06e5ba6750169f6269493d8c05427a9d92b01
Digvijaya Babu Bokinala <159063422+DigvijayaBabuBokinala721@users.noreply.github.com>
2025-08-20 11:44:49 +0530

    Delete EcommerceAPI directory

--- EcommerceAPI/src/main/java/com/hello/repository/OrdersRepo.java
+++
@@ -1,9 +0,0 @@
-package com.hello.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hello.entity.CustomerOrder;
-
-public interface OrdersRepo extends JpaRepository<CustomerOrder,Long> {
-
-}
